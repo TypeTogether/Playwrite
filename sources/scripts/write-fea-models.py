fad6cd608aedbb50bd4fcc69153eed67dce2cbe1
Joancarles Casasín <joanca@casasin.com>
2023-07-31 15:12:53 +0200

    Add idotless jdotless to models .csv and build new .fea models

--- sources/scripts/write-fea-models.py
+++ sources/scripts/write-fea-models.py
@@ -8,7 +8,7 @@ __doc__ = """
 
 
 UC_src = "@UC_src = [A B C D E F G H I J K L M N O P Q R S T U V W X Y Z];"
-lc_src = "@lc_src = [a b c d e f g h i j k l m n o p q r s t u v w x y z];"
+lc_src = "@lc_src = [a b c d e f g h i idotless j jdotless k l m n o p q r s t u v w x y z];"
 
 
 class langModel(object):
