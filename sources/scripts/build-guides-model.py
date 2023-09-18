94a0cf4976977cceed913f099fe6831d1c2510fb
Joancarles Casasín <joanca@casasin.com>
2023-09-18 21:47:03 +0200

    Add model guides scripts dev

---
+++ sources/scripts/build-guides-model.py
@@ -0,0 +1,59 @@
+import sys
+import os
+from fontParts.world import *
+# from fontTools.ttLib import TTFont
+
+
+def setFontInfo(font):
+    font.info.styleName = "Guides"
+    font.info.styleMapFamilyName += "Guides"
+    font.features.text = font.features.text.replace(
+        "(../features", "(../features")
+    font.save()
+
+
+def get_yGuides(font):
+    was = font.info.openTypeOS2WinAscent
+    capheight = font.info.capHeight
+    xheight = font.info.xHeight
+    baseline = 0
+    descender = - font.info.openTypeOS2WinDescent
+
+    return (capheight, xheight, baseline, descender)
+
+
+def drawGuides(glyph, yValues, lineWidth=10):
+    glyphWidth = glyph.width
+
+    if glyph.name != ".notdef":
+        glyph.clear(anchors=False)
+
+    if glyphWidth and glyph.name != ".notdef":
+        pen = glyph.getPen()
+        for y in yValues:
+            pen.moveTo((0, y))
+            pen.lineTo((glyphWidth, y)),
+            pen.lineTo((glyphWidth, y + lineWidth)),
+            pen.lineTo((0, y + lineWidth)),
+            pen.closePath()
+
+
+def main():
+    tag = sys.argv[1]
+    tagnospace = tag.replace('_', '')
+    src_path = os.path.abspath(f"./instance_ufo/Playpen{tagnospace}-Regular.ufo")
+    tgt_path = src_path.replace("Regular", "Guides")
+
+    src_font = OpenFont(src_path, showInterface=False)
+    src_font.save(tgt_path)
+
+    tgt_font = OpenFont(tgt_path, showInterface=False)
+    setFontInfo(tgt_font)
+    guides_y = get_yGuides(tgt_font)
+    for glyph in tgt_font:
+        drawGuides(glyph, guides_y)
+    tgt_font.save()
+
+
+if __name__ == "__main__":
+    main()
