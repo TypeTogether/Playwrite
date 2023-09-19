357ee285d99a33cd4a5686cd278a7bda1b04b47f
Joancarles Casasín <joanca@casasin.com>
2023-09-19 18:04:48 +0200

    Update ufo sources

--- sources/scripts/build-wght-model-var.py
+++ sources/scripts/build-wght-model-var.py
@@ -12,10 +12,11 @@ def main():
         models_tags = [tag for tag in tags if tag in mdd.keys()]
     # build each
     for tag in models_tags:
+        tagnospace = tag.replace("_", "")
         sl = mdd[tag]["slnt"]
         ex = mdd[tag]["EXTD"]
         sp = mdd[tag]["SPED"]
-        varfont = TTFont(f"./../../fonts-models/fonts-{tag}/variable/Playpen{tag}[wght,EXTD,SPED,slnt].ttf")
+        varfont = TTFont(f"./../../fonts-models/fonts-{tag}/variable/Playpen{tagnospace}[wght,EXTD,SPED,slnt].ttf")
         # fix hasItalics (ENG_Joined and Semijoined by now)
         has_sl = ""
         if type(sl) is str and "-" in sl:
@@ -34,11 +35,10 @@ def main():
         if sl != 0 and type(sl) != tuple:
             var_wght_only["post"].italicAngle = - int(sl)
         # tag in output without "_"
-        file_tag = tag.replace("_", "")
-        var_wght_only.save(f"./../../fonts-models/fonts-{tag}/variable/Playpen{file_tag}[wght{has_sl}].ttf")
+        var_wght_only.save(f"./../../fonts-models/fonts-{tag}/variable/Playpen{tagnospace}[wght{has_sl}].ttf")
         var_wght_only.close()
         varfont.close()
-        print(f"Instanced Playpen{file_tag}[wght{has_sl}].ttf")
+        print(f"Instanced Playpen{tagnospace}[wght{has_sl}].ttf")
 
 if __name__ == "__main__":
     main()
