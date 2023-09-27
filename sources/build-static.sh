250d517a79f1a42be06d99f561e3fe0a5a454e36
Joancarles Casasín <joanca@casasin.com>
2023-09-27 13:13:25 +0200

    Update PRT

--- sources/build-static.sh
+++ sources/build-static.sh
@@ -2,13 +2,12 @@
 
 set -e
 
-otfFontsPath="../fonts/static/otf"
 ttfFontsPath="../fonts/static/ttf"
 webFontsPath="../fonts/static/web"
 scripts="./scripts"
 
-rm -rf $ttfFontsPath # $otfFontsPath $webFontsPath
-mkdir -p $ttfFontsPath # $otfFontsPath $webFontsPath
+rm -rf $ttfFontsPath # $webFontsPath
+mkdir -p $ttfFontsPath # $webFontsPath
 
 # pack source as .glyphspackage is not supported yet by fontmake
 echo
@@ -22,26 +21,10 @@ echo "
  $(date "+ 📅 DATE: %Y-%m-%d%n  🕒 TIME: %H:%M:%S")"
 echo
 
-# # Build OTF fonts
-# fontmake -g ./Playpen_MM.glyphs -i -o otf --output-dir $otfFontsPath
-
 # Build TTF fonts
 fontmake -g ./Playpen_MM.glyphs -i -o ttf --output-dir $ttfFontsPath \
           --filter DecomposeTransformedComponentsFilter \
           --flatten-components
-
-# echo "
-# ======================
-#  Post processing OTFs 
-# ======================
-# "
-# otfs=$(ls $otfFontsPath/*.otf)
-# for otf in $otfs
-# do
-#     echo $otf
-#     python $scripts/fix-usWeightClass-otf.py $otf
-#     # psautohint --no-zones-stems -a $otf
-# done
 
 echo "
 ======================
@@ -57,5 +40,3 @@ do
   echo $ttf
 done
 
-# Clean up
-rm -rf ./master_ufo/ ./instance_ufo/
